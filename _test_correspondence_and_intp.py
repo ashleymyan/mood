12cd4ec98520cf8f3f34de59c7a7ee26d13f7afe
Huzheng Yang <huze.yann@gmail.com>
2025-11-21 09:14:14 +0100

    added IPA reconstruction and correspondence tests

--- _test_correspondence_and_intp.py
+++ _test_correspondence_and_intp.py
@@ -20,8 +20,8 @@ from extract_features import extract_dino_features, dino_image_transform
 from dino_correspondence import ncut_tsne_multiple_images, kway_cluster_per_image, match_centers_two_images
 images = torch.stack([dino_image_transform(image) for image in [image1, image2]])
 dino_image_embeds = extract_dino_features(images)
-n_clusters1 = 10
-n_clusters2 = 10
+n_clusters1 = 20
+n_clusters2 = 20
 cluster_eigenvectors1 = _kway_cluster_single_image(dino_image_embeds[0], n_clusters=n_clusters1, gamma=None)
 cluster_eigenvectors2 = _kway_cluster_single_image(dino_image_embeds[1], n_clusters=n_clusters2, gamma=None)
 a_to_b_mapping = match_centers_two_images(
@@ -54,3 +54,41 @@ ax2.imshow(b_cluster_image, cmap='tab20_r')
 ax3.imshow(image2)
 plt.show()
 #%%
+from app import perform_two_image_interpolation, train_mood_space
+config_path = "./config.yaml"
+model, trainer = train_mood_space(
+    pil_images=[image1, image2],
+    learning_rate=0.001, 
+    training_steps=1000,
+    mlp_width=512,
+    mlp_layers=4,
+    config_path=config_path,
+    n_eig=64,
+)
+# %%
+import numpy as np
+interpolation_weights = np.linspace(0.2, 1.0, 40).tolist()
+interpolated_images = perform_two_image_interpolation(
+    image1, 
+    image2, 
+    model,
+    interpolation_weights,
+    n_clusters=20, 
+    match_method='hungarian',
+    use_dino_matching=True,
+    config_path=config_path,
+    predefined_matching=([cluster_eigenvectors1, cluster_eigenvectors2], a_to_b_mapping)
+)
+all_images = interpolated_images
+# all_images = interpolated_images
+display_size = (512, 512)
+resized_images = [img.resize(display_size, Image.Resampling.LANCZOS) for img in all_images]
+result_grid = create_image_grid(resized_images, 10, len(resized_images)//10)
+result_grid
+# %%
+display(interpolated_images[13])
+display(interpolated_images[19])
+display(interpolated_images[25])
+display(interpolated_images[28])
+display(interpolated_images[32])
+# %%
