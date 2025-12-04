aaf8add893468bfe716bb87cfda5c08b2bec12fc
Huzheng Yang <huze.yann@gmail.com>
2025-12-04 07:24:10 +0100

    update feedback form link

--- app.py
+++ app.py
@@ -81,6 +81,8 @@ def create_gradio_interface():
         
         Given a pair of images, vibe blending will generate a set of images that creatively connect the input images.
         
+        **[📝 Feedback Form](https://docs.google.com/forms/d/e/1FAIpQLSfS-2fdJ3eaG6JBUGNgHYD4zNRtoPUOc2OhF8J-uT-gyR3LyA/viewform?usp=dialog)** - Please submit your interesting images!
+        
         """)
         with gr.Row():
             with gr.Column():
